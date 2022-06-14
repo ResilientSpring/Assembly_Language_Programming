59f93b4dada6f30c5cfaf9fcd070fdfebe8ed7dd
Ceiling_roof <ted10014@hotmail.com>
2022-06-14 14:00:38 +0800

    Committed 2022/06/14

--- Chapter 13 - IDE and C++/DemoMasm/sum.asm
+++ Chapter 13 - IDE and C++/DemoMasm/sum.asm
@@ -1,7 +1,17 @@
 ; Subroutine thesum adds a variable number of integers.
-; RCX:Memory address of variable to receive the sum.
-; RDX: Memory address of array of integer values
-; R8: Number of integers in the array
-; Supports X64 Calling Convention
+;	RCX:Memory address of variable to receive the sum.
+;	RDX: Memory address of array of integer values
+;	R8: Number of integers in the array
+;	Supports X64 Calling Convention
 
-.code
+	   .code
+thesum  proc              ; Subroutine thesum entry point
+		mov EAX, [RDX]    ; Load first value.
+		dec R8            ; Decrement number of integers.
+		jle retsub		  ; Return with just one alue.
+thelp:  add EAX, [RDX+4*R8]   ; Add next integer to running t
+		dec R8		; Numer of integers still to add.
+		jnz thelp		  ; Continue with next integer
+retsub: mov [RCX], EAX	  ; Return sum to calling program
+		ret				  ; Return to the calling program
+thesum  endp
