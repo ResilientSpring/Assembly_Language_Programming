b5b54a3b7481aa3e3e12315a1355910d7a13c76a
Ceiling_roof <ted10014@hotmail.com>
2022-06-14 13:53:59 +0800

    Committed 2022/06/14

---
+++ Chapter 13 - IDE and C++/DemoMasm/sum.asm
@@ -0,0 +1,7 @@
+; Subroutine thesum adds a variable number of integers.
+; RCX:Memory address of variable to receive the sum.
+; RDX: Memory address of array of integer values
+; R8: Number of integers in the array
+; Supports X64 Calling Convention
+
+.code
