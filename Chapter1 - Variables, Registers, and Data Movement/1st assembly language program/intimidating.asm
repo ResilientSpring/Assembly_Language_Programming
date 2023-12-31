5ed477f04d9733f68308634350047d633da5081a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-01 00:02:02 +0800

    Committed on or around 2024/01/01

--- Chapter1 - Variables, Registers, and Data Movement/1st assembly language program/intimidating.asm
+++ Chapter1 - Variables, Registers, and Data Movement/1st assembly language program/intimidating.asm
@@ -1,2 +1,11 @@
 .686
-.mo
+.model flat, c
+.stack 100h
+.data
+
+num1 sdword ?  ; first number
+num2 sdword ?  ; second number
+     .code
+main proc
+     mov num1,5     ; initialize num1 with 5
+     mov eax, num1  ; load eax with contents
