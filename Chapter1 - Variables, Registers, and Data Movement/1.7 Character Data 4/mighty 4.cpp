aa53de5f11736d4ad59c66bee9a35f0c11f35788
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-01 17:53:31 +0800

    Committed on or around 2024/01/01

--- Chapter1 - Variables, Registers, and Data Movement/1.7 Character Data 4/mighty 4.cpp
+++ Chapter1 - Variables, Registers, and Data Movement/1.7 Character Data 4/mighty 4.cpp
@@ -11,8 +11,8 @@ int main() {
 		mov eax, letter1
 		mov letter2, eax
 
-	}
+	} // error, eax is 64-bit but embedded assembly only accepts 32-bit.
 
-	cout << "letter2 is " << letter2 << endl;
-
+	cout << "letter2 is " << letter2 << endl;  
+	
 }
