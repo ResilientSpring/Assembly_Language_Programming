8479bd26488f5e6ff3a2a8daa8938e8e48986bf5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-01 17:44:53 +0800

    Committed on or around 2024/01/01

--- Chapter1 - Variables, Registers, and Data Movement/1.7 Character Data 3/mighty 3.cpp
+++ Chapter1 - Variables, Registers, and Data Movement/1.7 Character Data 3/mighty 3.cpp
@@ -6,5 +6,16 @@ int main() {
 	char grade1;
 	char grade2 = 'A';
 	const char grades[10] = "ABC";
+	const char name[10] = "Abe";
+
+	char letter1 = 'A', letter2;
+
+	__asm {
+	
+		mov letter2, letter1;
+	
+	}
+	
+	cout << "letter2 is " << letter2 << endl;
 
 }
