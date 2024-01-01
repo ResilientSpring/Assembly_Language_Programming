2d3a3c5b47c274a2741cf2538e720d1056948d97
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-01 15:53:23 +0800

    Committed on or around 2024/01/01

--- Chapter2 Input Output/Hello World assembly/mighty.asm
+++ Chapter2 Input Output/Hello World assembly/mighty.asm
@@ -2,4 +2,14 @@
 .model flat, c
 .stack 100h
 
-printf PROTO arg1:PTR BYTE
+printf PROTO arg1:Ptr Byte
+.data
+
+msg1   byte "Hello World!", 0Ah, 0
+
+.code
+main proc
+INVOKE printf, ADDR msg1
+
+main endp
+     end
