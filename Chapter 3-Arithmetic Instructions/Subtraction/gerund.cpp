493214329703dcc8623f4412c27a5fa4b878be82
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-18 22:12:03 +0800

    Committed on or around 2024/01/18

---
+++ Chapter 3-Arithmetic Instructions/Subtraction/gerund.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int difference, num1 = 5, num2 = 7;
+
+	__asm {
+
+		; difference = num2 - num1
+
+		mov eax, num2;   move num2 into eax
+
+
+	}
+
+
+}
