7343809d79bd2d8db842b378c799b012aa3a4fba
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-19 23:44:08 +0800

    Committed on or around 2024/01/19

--- Chapter 3-Arithmetic Instructions/multiplication/imul.cpp
+++ Chapter 3-Arithmetic Instructions/multiplication/imul.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main() {
 
+	int num1, num2, result;
+
+	__asm {
+
+		mov num1, 5;
+		mov num2, 7;
+
+		imul result, num1, num2;
+
+	}
 
 
+	cout << "num1 x num2 = " << result << endl;
 
 }
