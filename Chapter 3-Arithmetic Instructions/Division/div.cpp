bf3864dced2549eb3c9c237f62a779d304bee1da
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-20 00:05:14 +0800

    Committed on or around 2024/01/20

--- Chapter 3-Arithmetic Instructions/Division/div.cpp
+++ Chapter 3-Arithmetic Instructions/Division/div.cpp
@@ -12,10 +12,13 @@ int main() {
 
 		mov eax, num1;
 
-		div num2;
+		div num2;   This divides the value in the dividend register(s)by divisor [1]
 
 		mov result, eax; 
 	}
 
 	cout << "num1 / num2 = " << result << endl;
 }
+
+// Reference:
+// 1. https://en.wikibooks.org/wiki/X86_Assembly/Arithmetic
