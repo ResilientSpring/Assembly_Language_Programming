356ee64db6d1115d35611dabe4a05054991f496f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-03 23:50:10 +0800

    Committed on or around 2024/03/03

---
+++ Chapter 3-Arithmetic Instructions/idiv  2/signed div  2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int answer;
+	int number;
+	int amount;
+
+	__asm {
+
+		; answer = number / amount
+
+		mov number, 5;
+
+		mov amount, 2;
+
+		mov eax, number;
+
+		cdq;  convert double word into quad word
+
+			idiv amount;  divide(edx : eax) by amount
+
+			mov answer, eax;
+	}
+
+	cout << answer << endl;
+
+}
