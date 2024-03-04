9a0171a96e8f1b6f0f6a0a87aa5779246d379d37
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-04 15:39:16 +0800

    Committed on or around 2024/03/04

--- Chapter 3-Arithmetic Instructions/sub 2/sub 2.cpp
+++ Chapter 3-Arithmetic Instructions/sub 2/sub 2.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main() {
 
-	int result
+	int difference, num1, num2;
 
+	__asm{
+
+		mov eax, 27;
+		sub eax, 15;
+		mov difference, eax;
+	}
+
+	cout << "27 - 15 = " << difference << endl;
 }
