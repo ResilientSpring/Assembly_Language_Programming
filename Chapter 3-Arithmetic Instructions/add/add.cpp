9f43e69a24a3dee2e8904453e4eabee3c5d4e877
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-04 14:18:48 +0800

    Committed on or around 2024/03/04

---
+++ Chapter 3-Arithmetic Instructions/add/add.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int sum, num1, num2;
+
+	__asm {
+
+		mov num1, 5;
+		mov num2, 7;
+
+		mov eax, num1;
+
+		add eax, num2; add num2 to eax
+
+		mov sum, eax; 
+
+	}
+
+	cout << "sum is " << sum << endl;
+}
