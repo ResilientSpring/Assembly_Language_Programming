577e97f83708b2159a97823fb655247388170058
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-05 23:44:34 +0800

    Committed on or around 2024/03/05

--- Chapter 3-Arithmetic Instructions/imul  2/Signed mul  2.cpp
+++ Chapter 3-Arithmetic Instructions/imul  2/Signed mul  2.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main() {
 
+	int product;
+	
+	// If num1 instead contained a negative 2 and num2 still contained a positive 5,
+	int num1 = -2; 
+	int num2 = 5;
 
+	__asm {
+
+		// The way the one-operand version of the signed multiplication instruction works is that 
+		// the eax register must first be loaded with the number that needs to be multiplied.
+
+
+
+	}
 
 }
