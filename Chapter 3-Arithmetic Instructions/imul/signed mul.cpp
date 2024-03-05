21a76ca3f804d8646a73d18e13b8cce8800cc644
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-05 23:14:45 +0800

    Committed on or around 2024/03/05

--- Chapter 3-Arithmetic Instructions/imul/signed mul.cpp
+++ Chapter 3-Arithmetic Instructions/imul/signed mul.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
 
-	int product, num1, num2;
+	int product, num1 = 2, num2 = 5;
 
 	__asm {
 
 		; product = num1 * num2
+
+		mov eax, num1;  eax register must first be loaded with the number that needs to be multiplied.
 
 	}
 
