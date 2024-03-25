96c2e181b91ce167d7c7c619c1f0017b72dbcda1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-25 23:31:19 +0800

    Committed on or around 2024/03/25

--- Chapter 3-Arithmetic Instructions/negation/precedence.cpp
+++ Chapter 3-Arithmetic Instructions/negation/precedence.cpp
@@ -4,15 +4,20 @@ using namespace std;
 int main() {
 
 	int x;
-	int y;
-	int z;
+	int y = 3;
+	int z = 4;
 
 	__asm {
 
 		; x = -y + z;
 
-		mov ea
+		neg y;
+		mov eax, y;
+		add eax, z;
+		mov x, eax;
 
 	}
 
+	cout << "x = -y + z; " << endl;
+	cout << "x is now " << x << endl;
 }
