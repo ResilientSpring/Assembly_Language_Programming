b15e900d63b4ddb521c849321c61e91447af0196
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-05 17:22:27 +0800

    Committed on or around 2024/04/05

---
+++ Chapter 3-Arithmetic Instructions/imul 4/Signed mul 4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int product, num1, num2;
+
+	__asm {
+
+		mov num1, -2;
+		mov num2, 5;
+
+		mov eax, num1;
+
+		imul 5;  // Note that with the one-operand imul instruction, there is no provision for an immeidate operand and that the use of the eax register for the multiplicand is implied.
+
+		mov product, eax;
+
+	}
+
+	cout << "-2 x 5 = " << product << endl;
+}
