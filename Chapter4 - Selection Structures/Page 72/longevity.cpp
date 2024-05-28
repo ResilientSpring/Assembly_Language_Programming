ad0f3dc65dd6a62bb342116c7011c5e95674769d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-05-28 13:54:20 +0800

    Committed on or around 2024/05/28

---
+++ Chapter4 - Selection Structures/Page 72/longevity.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int number = 5, amount = 8;
+
+	if (number == 0)
+		number--;
+
+	// number is still 5.
+	__asm {
+
+		cmp number, 5;
+		je then_01;
+		jne end_if_01;
+		then_01 : dec number;
+		end_if_01: nop;
+	}
+
+	cout << number << endl;
+}
